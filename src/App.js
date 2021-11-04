471e812025089cbe7d3e1563ec4e0b4c0a7fd6e3
eygis <86146074+eygis@users.noreply.github.com>
2021-11-04 18:57:07 +0900

    21/11/4 basic banner and footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends React.Component {
   
     return (
     <div className="App">
-      pokedex
+      <div id="banner">
+      <h1><a href="https://fontmeme.com/pokemon-font/"><img src="https://fontmeme.com/permalink/211104/a930b2e32421ad097d5a5dcdedaa93f0.png" alt="pokemon-title" /></a></h1>
+      </div>
+      <div id="footer">Pokémon and Pokémon character names are trademarks of Nintendo.</div>
     </div>
   );
 }
